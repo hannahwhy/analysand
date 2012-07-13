c5e53fa8a4a6bfb3d134875b2afd6b8c34fc25f4
David Yip <yipdw@member.fsf.org>
2012-07-13 02:06:51 -0500

    We don't need to use condition variables anymore.  #22.
    The #waiter_for method, as hacktastic as it is, seems to be fulfilling
    its intended purpose.

--- spec/couchdb/change_watcher_spec.rb
+++ spec/couchdb/change_watcher_spec.rb
@@ -1,21 +1,18 @@
 require 'spec_helper'
 
 require 'couchdb/change_watcher'
-require 'thread'
 
 module Couchdb
   describe ChangeWatcher do
     class TestWatcher < Couchdb::ChangeWatcher
       attr_accessor :changes
 
-      def initialize(database, credentials, mutex, cond)
+      def initialize(database, credentials)
         super(database)
 
         self.changes = []
 
         @credentials = credentials
-        @mutex = mutex
-        @cond = cond
       end
 
       def customize_request(req)
@@ -25,13 +22,8 @@ module Couchdb
       def process(change)
         changes << change
         change_processed(change)
-
-        @mutex.synchronize { @cond.signal }
       end
     end
-
-    let!(:mutex) { Mutex.new }
-    let!(:ready) { ConditionVariable.new }
 
     let(:database_name) { "catalog_database_#{Rails.env}" }
     let(:database_uri) { instance_uri + "/#{database_name}" }
@@ -45,18 +37,8 @@ module Couchdb
       drop_databases!
     end
 
-    it 'receives changes' do
-      watcher = TestWatcher.new(db, admin_credentials, mutex, ready)
-
-      db.put('foo', { 'foo' => 'bar' }, admin_credentials)
-
-      mutex.synchronize { ready.wait(mutex, 1) }
-
-      watcher.changes.select { |r| r['id'] == 'foo' }.length.should == 1
-    end
-
     describe '#waiter_for' do
-      let!(:watcher) { TestWatcher.new(db, admin_credentials, mutex, ready) }
+      let!(:watcher) { TestWatcher.new(db, admin_credentials) }
 
       describe 'if the given document has not been processed' do
         it 'blocks until the document has been processed' do
@@ -72,6 +54,16 @@ module Couchdb
           watcher.changes.detect { |r| r['id'] == 'foo' }.should_not be_nil
         end
       end
+    end
+
+    it 'receives changes' do
+      watcher = TestWatcher.new(db, admin_credentials)
+
+      waiter = watcher.waiter_for('foo')
+      db.put('foo', { 'foo' => 'bar' }, admin_credentials)
+      waiter.wait
+
+      watcher.changes.select { |r| r['id'] == 'foo' }.length.should == 1
     end
   end
 end
